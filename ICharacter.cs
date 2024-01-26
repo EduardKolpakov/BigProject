11d2e57f81bfa469f050b5821bc4487f04eba9b3
EduardKolpakov <94556805+EduardKolpakov@users.noreply.github.com>
2024-01-26 23:26:34 +0300

    first part of files

---
+++ ICharacter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCharacters
+{
+    internal interface ICharacter
+    {
+        public int Strength
+        { get; set; }
+        public int Vitality
+        { get; set; }
+        public int Inteligence
+        { get; set; }
+        public int Dexterity
+        { get; set; }
+        public int Health
+        { get; set; }
+        public int MaxHealth
+        { get; set; }
+        public int Mana
+        { get; set; }
+        public int MaxMana
+        { get; set; }
+
+        public void addVital(int points)
+        {
+            Vitality += points;
+        }
+    }
+}
