23fc2cfd5aa66bae227ddb2d826f28b8ab3edb25
Kolpa <Kolpa@JARVIS>
2024-02-12 21:38:42 +0300

    staff added (2.0.2 update)

---
+++ WPFCharacters/NoWeapon.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFCharacters
+{
+    internal class NoWeapon
+    {
+        private string _qual;
+        private string _name;
+        private double _pdmg_b;
+        private double _mana_b;
+        private double _int_b;
+        private int _cc_b;
+        private int _cd_b;
+        private double _str_b;
+        private double _dex_b;
+        private bool _shield;
+        private double _hp_b;
+        public NoWeapon(string qual)
+        {
+            Quality = qual;
+            Name = "Staff";
+        }
+
+        public string Quality
+        {
+            get { return _qual; }
+            set
+            {
+                _qual = value;
+                PDmg_b = 1;
+                Mana_b = 1;
+                Int_b = 1;
+                CC_b = 0;
+                CD_b = 0;
+                Dex_b = 1;
+                hp_b = 1;
+                Str_b = 1;
+                Shield = false;
+            }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public double PDmg_b
+        {
+            get { return _pdmg_b; }
+            set { _pdmg_b = value; }
+        }
+        public double Mana_b
+        {
+            get { return _mana_b; }
+            set { _mana_b = value; }
+        }
+        public double Int_b
+        {
+            get { return _int_b; }
+            set { _int_b = value; }
+        }
+        public int CC_b
+        {
+            get { return _cc_b; }
+            set { _cc_b = value; }
+        }
+        public int CD_b
+        {
+            get { return _cd_b; }
+            set { _cd_b = value; }
+        }
+        public double Str_b
+        {
+            get { return _str_b; }
+            set { _str_b = value; }
+        }
+        public double Dex_b
+        {
+            get { return _dex_b; }
+            set { _dex_b = value; }
+        }
+        public bool Shield
+        {
+            get { return _shield; }
+            set { _shield = value; }
+        }
+        public double hp_b
+        {
+            get { return _hp_b; }
+            set { _hp_b = value; }
+        }
+    }
+}
--- WPFCharacters/obj/Debug/net6.0-windows/WPFCharacters.AssemblyInfo.cs
+++ WPFCharacters/obj/Debug/net6.0-windows/WPFCharacters.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WPFCharacters")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c09a30aaa646e741e732f2d8fea7ed75e02a5442")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cad1328fae61515910c72ae56b811533527ba2b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WPFCharacters")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WPFCharacters")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
